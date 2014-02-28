e799aa1abe27ee3dd445ac7721e88ea9177861c4
clownycashew <aarther97@gmail.com>
2014-02-28 14:45:50 -0500

    Update days.js

--- days.js
+++ days.js
@@ -1,11 +1,3 @@
-var d = new Date();
-var n = d.getDay();
 
-if (n = 2) 
-	{
-	document.write(<h2> Monday </h2>)
-	};
-if (n = 3)
-	{
-	document.write(<h2> Tuesday </h2>)
-	};
+
+simply.title('Hello World!');
