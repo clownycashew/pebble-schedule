b0d1be72d55a801fe7ade475b6db1a006d05b48d
clownycashew <aarther97@gmail.com>
2014-02-28 17:01:28 -0500

    Update days.js

--- days.js
+++ days.js
@@ -2,7 +2,7 @@ var date = new Date();
 var day = date.getDay();
 
 var name = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"][day];
-var n = weekday[d.getDay()];
+
 
 if (day = "Monday" );
   { 
