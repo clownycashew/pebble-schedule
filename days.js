1ad3e34146eddc6f28b01c34cfd99d357ac48b95
clownycashew <aarther97@gmail.com>
2014-02-28 14:49:06 -0500

    Update days.js

--- days.js
+++ days.js
@@ -1,3 +1,13 @@
+var d=new Date();
+var weekday=new Array(7);
+weekday[0]="Sunday";
+weekday[1]="Monday";
+weekday[2]="Tuesday";
+weekday[3]="Wednesday";
+weekday[4]="Thursday";
+weekday[5]="Friday";
+weekday[6]="Saturday";
 
+var n = weekday[d.getDay()];
 
-simply.title('Hello World!');
+simply.title( n );
