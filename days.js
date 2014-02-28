5cbdc0d50f163fb2b029e4cb4847ea28961daaac
clownycashew <aarther97@gmail.com>
2014-02-28 18:08:08 -0500

    Update days.js

--- days.js
+++ days.js
@@ -1,8 +1,14 @@
-var date = new Date();
-var day = date.getDay();
+var d=new Date();
+var weekday=new Array(7);
+weekday[0]="Sunday";
+weekday[1]="Monday";
+weekday[2]="Tuesday";
+weekday[3]="Wednesday";
+weekday[4]="Thursday";
+weekday[5]="Friday";
+weekday[6]="Saturday";
 
-var name = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"][day];
-
+var n = weekday[d.getDay()];
 
 if (name = "Monday" );
   { 
