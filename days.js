825dfbb5b3279959e38c93086402d9bfa6dc6bae
clownycashew <aarther97@gmail.com>
2014-02-28 16:52:54 -0500

    Update days.js

--- days.js
+++ days.js
@@ -1,11 +1,6 @@
 (function() {
     var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
 
-    var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
-
-    Date.prototype.getMonthName = function() {
-        return months[ this.getMonth() ];
-    };
     Date.prototype.getDayName = function() {
         return days[ this.getDay() ];
     };
