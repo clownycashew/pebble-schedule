5cc945004c0d3b5310b8abdb0cdd3f72c15b87f1
clownycashew <aarther97@gmail.com>
2014-02-28 16:51:41 -0500

    Update days.js

--- days.js
+++ days.js
@@ -1,36 +1,41 @@
-var d=new Date();
-var weekday=new Array(7);
-weekday[0]="Sunday";
-weekday[1]="Monday";
-weekday[2]="Tuesday";
-weekday[3]="Wednesday";
-weekday[4]="Thursday";
-weekday[5]="Friday";
-weekday[6]="Saturday";
+(function() {
+    var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
 
-var n = weekday[d.getDay()];
+    var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
 
-if (n = "Monday" );
+    Date.prototype.getMonthName = function() {
+        return months[ this.getMonth() ];
+    };
+    Date.prototype.getDayName = function() {
+        return days[ this.getDay() ];
+    };
+})();
+
+var now = new Date();
+
+var day = now.getDayName();
+
+if (day = "Monday" );
   { 
   simply.title( n ,
   'Regular Schedule');
   }
-if (n = "Tuesday");
+if (day = "Tuesday");
   { 
   simply.title( n ,
   'Explorer Schedule');
   }
-if (n = "Wednesday");
+if (day = "Wednesday");
   { 
   simply.title( n ,
   'Block Schedule');
   }
-if (n = "Thursday");
+if (day = "Thursday");
   { 
   simply.title( n ,
   'Block Schedule');
   }
-if (n = "Friday");
+if (day = "Friday");
   { 
   simply.title( n ,
   'Regular Schedule');
