0c757e381976a1570989abade8207f5053fa5338
clownycashew <aarther97@gmail.com>
2014-02-28 16:48:58 -0500

    Update days.js

--- days.js
+++ days.js
@@ -1,27 +1,36 @@
-var d = new Date();
-var n = d.getDay();
+var d=new Date();
+var weekday=new Array(7);
+weekday[0]="Sunday";
+weekday[1]="Monday";
+weekday[2]="Tuesday";
+weekday[3]="Wednesday";
+weekday[4]="Thursday";
+weekday[5]="Friday";
+weekday[6]="Saturday";
 
-if (n = 2 );
+var n = weekday[d.getDay()];
+
+if (n = "Monday" );
   { 
   simply.title( n ,
   'Regular Schedule');
   }
-if (n = 3);
+if (n = "Tuesday");
   { 
   simply.title( n ,
   'Explorer Schedule');
   }
-if (n = 3);
+if (n = "Wednesday");
   { 
   simply.title( n ,
   'Block Schedule');
   }
-if (n = 4);
+if (n = "Thursday");
   { 
   simply.title( n ,
   'Block Schedule');
   }
-if (n = 5);
+if (n = "Friday");
   { 
   simply.title( n ,
   'Regular Schedule');
